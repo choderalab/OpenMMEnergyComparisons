c3cc4ebcd346bd36a2ebb3af4fa69ab0a3a401b2
Peter Eastman <peter.eastman@gmail.com>
2016-08-05 11:34:38 -0700

    Python 3 compatibility

--- Gromacs/TestGromacsForces.py
+++ Gromacs/TestGromacsForces.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from simtk.openmm import *
 from simtk.openmm.app import *
 from simtk.unit import *
@@ -6,13 +7,13 @@ import subprocess
 import re
 
 def printUsage():
-    print "TestGromacsForces.py requires at least one argument of the form filename=(filename)"
-    print "filename should be the name (without extension) of the top/gro/mdp files to load"
-    print "It also accepts additional arguments of the form argumentName=value:"
-    print "platform:  A platform (Reference, CPU, OpenCL, or CUDA); default is platform=Reference"
-    print "precision: The precision to use (single, mixed, or double); default is single"
-    print
-    print "Example:   python TestGromacsForces.py filename=dhfr.pbc platform=cuda"  
+    print("TestGromacsForces.py requires at least one argument of the form filename=(filename)")
+    print("filename should be the name (without extension) of the top/gro/mdp files to load")
+    print("It also accepts additional arguments of the form argumentName=value:")
+    print("platform:  A platform (Reference, CPU, OpenCL, or CUDA); default is platform=Reference")
+    print("precision: The precision to use (single, mixed, or double); default is single")
+    print()
+    print("Example:   python TestGromacsForces.py filename=dhfr.pbc platform=cuda")
 
 # Set default values
 
@@ -36,13 +37,13 @@ for arg in sys.argv[1:]:
 # Ensure that the argument filename is provided
 
 if filename is None:
-    print "Error: Argument list must contain a filename"
+    print("Error: Argument list must contain a filename")
     printUsage()
     sys.exit(1) 
 
-print "filename =", filename
-print "platformName =", platformName
-print "precision =", precision
+print("filename =", filename)
+print("platformName =", platformName)
+print("precision =", precision)
 
 if platformName == 'OpenCL':
     properties = {'OpenCLPrecision':precision}
@@ -90,7 +91,7 @@ for line in open(filename+'.mdp'):
 if mdpOptions.get('implicit_solvent') == 'gbsa':
     system = top.createSystem(nonbondedMethod=NoCutoff, implicitSolvent=OBC2)
 elif mdpOptions.get('coulombtype') == 'pme':
-    system = top.createSystem(nonbondedMethod=PME, ewaldErrorTolerance=5e-5)
+    system = top.createSystem(nonbondedMethod=PME, ewaldErrorTolerance=5e-5, nonbondedCutoff=0.9*nanometers)
 else:
     system = top.createSystem(nonbondedMethod=NoCutoff)
 for i,f in enumerate(system.getForces()):
@@ -106,23 +107,23 @@ forces = state.getForces().value_in_unit(kilojoules_per_mole/nanometer)
 
 relativeDiff = [2*norm(f1-f2)/(norm(f1)+norm(f2)) for f1, f2 in zip(forces, gromacsForces)]
 relativeDiff.sort()
-print 'Relative force error'
-print 'max:', relativeDiff[-1]
-print 'mean: ', sum(relativeDiff)/len(relativeDiff)
-print 'median:', relativeDiff[len(relativeDiff)/2]
+print('Relative force error')
+print('max:', relativeDiff[-1])
+print('mean: ', sum(relativeDiff)/len(relativeDiff))
+print('median:', relativeDiff[len(relativeDiff)/2])
 projection = [dot(f1, f2)/dot(f1, f1) for f1, f2 in zip(forces, gromacsForces)]
-print
-print 'Projection'
-print 'mean:', sum(projection)/len(projection)
-print 'min:', min(projection)
-print 'max:', max(projection)
-print
-print 'OpenMM component energies'
+print()
+print('Projection')
+print('mean:', sum(projection)/len(projection))
+print('min:', min(projection))
+print('max:', max(projection))
+print()
+print('OpenMM component energies')
 for i,f in enumerate(system.getForces()):
     state = simulation.context.getState(getEnergy=True, groups={i})
-    print f.__class__, state.getPotentialEnergy()
-print
-print 'Gromacs component energies'
+    print(f.__class__, state.getPotentialEnergy())
+print()
+print('Gromacs component energies')
 for term in termEnergies:
-    print term, termEnergies[term]
+    print(term, termEnergies[term])
 
